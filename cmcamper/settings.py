3b50e508b1aaae38b73880625bccda27838a0790
Joel Allara <jallara@ams.red>
2021-03-13 16:54:38 -0300

    Configuraciones Heroku

--- cmcamper/settings.py
+++ cmcamper/settings.py
@@ -142,6 +142,6 @@ STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
