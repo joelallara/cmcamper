7237b4b0099bdcbfce9dd59d4b6f5c000b1fc19a
Joel Allara <jallara@ams.red>
2021-03-13 16:39:13 -0300

    Configuraciones Heroku

--- cmcamper/settings.py
+++ cmcamper/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'cmcamper.wsgi.application'
 #         }
 #     }
 # else:
-    import dj_database_url
-    from decouple import config
+import dj_database_url
+from decouple import config
 
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL')
-        )
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
